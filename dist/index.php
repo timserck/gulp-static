8f9887d3815fe7dbe019ef50c2e54a3a051e2ccd
T.Serck <timserck>
2017-07-16 12:09:48 +0200

    new pug extention

--- dist/index.php
+++ dist/index.php
@@ -1,122 +1,69 @@
-<?php include_once("inc/meta.inc.php"); ?>
-
-<title></title>
-<meta name="description" content="" />
-
-</head><body>
-
-<?php include_once("inc/analyticstracking.inc.php"); ?>
-
-<?php include_once("inc/header.inc.php"); ?>  
-<?php include_once("inc/slider.inc.php"); ?>  
-
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<title>test</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="imgs/icons/favicons/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+	<meta name="description" content="description">
+	<meta name="keywords" content="test, test, test">
+	<meta name="author" content="Timothée Serck">
+	<meta name="google" content="notranslate">
+	<meta property="og:title" content="facebook">
+	<meta property="og:image" content="http://www.timotheeserck.be/projets/drSerck/imgs/icons/og_image.jpg">
+	<meta property="og:url" content="http://www.timotheeserck.be/projets/drSerck/">
+	<meta property="og:description" content="faceDescription">
+	<meta property="og:locale">
+	<meta property="og:type" content="website">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:creator" content="@Serck_Timothee">
+	<link rel="apple-touch-icon" sizes="57x57" href="imgs/icons/favicons/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="imgs/icons/favicons/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="imgs/icons/favicons/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="imgs/icons/favicons/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="imgs/icons/favicons/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="imgs/icons/favicons/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="imgs/icons/favicons/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="imgs/icons/favicons/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="imgs/icons/favicons/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="imgs/favicons/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="imgs/icons/favicons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="imgs/icons/favicons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="imgs/icons/favicons/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<link rel="stylesheet" type="text/css" href="css/style.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Merriweather:400,700,900|Oswald:300,400,700">
+</head>
+<nav class="grid navigation" role="navigation">
+	<div class="navigation-wrap row">
+		<div class="navigation-wrap--inner col-12-16"><span class="logo"></span></div>
+		<ul class="navigation-menu navigation-wrap--inner col-4-16 row show" id="js-navigation-menu">
+			<div class="ham"></div>
+			<li class="col-4-16 nav-link"><a href="javascript:void(0)">5</a></li>
+			<li class="col-4-16 nav-link"><a href="javascript:void(0)">2</a></li>
+			<li class="col-4-16 nav-link"><a href="javascript:void(0)">3</a></li>
+			<li class="col-4-16 nav-link"><a href="javascript:void(0)">4</a></li>
+		</ul>
+	</div>
+</nav>
 <main class="main">
-  <div class="content">
-    
-  
-<section class="hero">
-  
-  <h1>Bitters example page</h1>
-
-  <p>The purpose of this HTML is to help determine what default settings are with Bitters and to make sure that all possible HTML Elements are included in this HTML so as to not miss any possible Elements when designing a site.</p>
-  
-
-  
-</section>
- 
-<section class="headings">
-
-
-  <h1 >Headings</h1>
-
-  <h1>h1. Heading</h1>
-  <h2>h2. Heading</h2>
-  <h3>h3. Heading</h3>
-  <h4>h4. Heading</h4>
-  <h5>h5. Heading</h5>
-  <h6>h6. Heading</h6>
-  
-</section>
-
-<section class="paragraph">
-
-<h1 >Paragraph</h1>
-
-  <p>Lorem ipsum dolor sit amet, <a href="#" title="test link">test link</a> adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-  <p>Lorem ipsum dolor sit amet, <em>emphasis</em> consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-  
-</section>
-
-
-
-<section class="last">
-    <p>Lorem ipsum dolor sit amet, <a href="#" title="test link">test link</a> adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-  <p>Lorem ipsum dolor sit amet, <em>emphasis</em> consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-</section>
-
-  </div>
-
-
-  <div class="slider">test</div>
-
-  <div class="content">
-
-
-  <section class="hero">
-  
-  <h1>Bitters example page</h1>
-
-  <p>The purpose of this HTML is to help determine what default settings are with Bitters and to make sure that all possible HTML Elements are included in this HTML so as to not miss any possible Elements when designing a site.</p>
-
-  
-</section>
- 
-<section class="headings">
-
-
-  <h1 >Headings</h1>
-
-  <h1>h1. Heading</h1>
-  <h2>h2. Heading</h2>
-  <h3>h3. Heading</h3>
-  <h4>h4. Heading</h4>
-  <h5>h5. Heading</h5>
-  <h6>h6. Heading</h6>
-  
-</section>
-
-<section class="paragraph">
-
-<h1 >Paragraph</h1>
-
-  <p>Lorem ipsum dolor sit amet, <a href="#" title="test link">test link</a> adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-  <p>Lorem ipsum dolor sit amet, <em>emphasis</em> consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-  
-</section>
-
-
-
-<section class="last">
-    <p>Lorem ipsum dolor sit amet, <a href="#" title="test link">test link</a> adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-
-  <p>Lorem ipsum dolor sit amet, <em>emphasis</em> consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus.</p>
-</section>
-
-
-    
-  </div>
-
-
-
-
-
-
+	<div class="slider">
+		<div class="content">
+			<section class="hero">
+				<h1>locals.titre</h1>
+			</section>
+		</div>
+	</div>
 </main>
-
-
-<?php include_once("inc/footer.inc.php"); ?>
+<footer class="footer" role="contentinfo"></footer>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-49600808-3', 'auto');
+	ga('send', 'pageview');
+</script>
+<script src="js/all.js"></script>
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 // Include gulp
-var gulp = require('gulp'); 
+var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
@@ -13,17 +13,50 @@ var image = require('gulp-image');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var babel = require('gulp-babel');
-var newer  = require('gulp-newer');
-
+var newer = require('gulp-newer');
+var pug = require('gulp-pug');
+var data = require('gulp-data');
+var fs = require("fs");
+var pugPHPFilter = require('pug-php-filter');
+var watch = require('gulp-watch');
+var svgSprite = require('gulp-svg-sprite');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var bourbon = require('node-bourbon').includePaths;
-var neat = require('node-neat').includePaths;
+// var neat = require('node-neat').includePaths;
+
+var swallowError = function swallowError(error) {
+
+    // If you want details of the error in the console
+    console.log(error.toString())
+
+    this.emit('end')
+}
 
 
-
+gulp.task('pug', function buildHTML() {
+    return gulp.src('src/views/index.pug')
+        .pipe(data(function(file) {
+            return JSON.parse(
+                fs.readFileSync('src/views/data.json')
+            );
+        }))
+        .on("error", function(err) { console.log("Error : " + err.message); })
+        .pipe(pug({
+            // Your options in here.
+            pretty: "\t",
+            filters: {
+                php: pugPHPFilter
+            }
+        }))
+        .on('error', swallowError)
+        .pipe(rename(function(path) {
+            path.extname = ".php"
+        }))
+        .pipe(gulp.dest('dist'));
+});
 
 // Lint Task
 gulp.task('lint', function() {
@@ -34,29 +67,29 @@ gulp.task('lint', function() {
 
 // Compile and Minify Our Sass
 gulp.task('sass', function() {
-     var onError = function(error) {
+    var onError = function(error) {
         notify.onError({
-                    title:    "GULP",
-                    subtitle: "Erreur(s) détectée(s) ! Gulp n'a pas su effectuer la tâche demandée",
-                    message:  "Error: <%= error.message %>",
-                    icon: false,
-                    sound: false
-                })(error);
+            title: "GULP",
+            subtitle: "Erreur(s) détectée(s) ! Gulp n'a pas su effectuer la tâche demandée",
+            message: "Error: <%= error.message %>",
+            icon: false,
+            sound: false
+        })(error);
 
         this.emit('end');
     };
 
     return gulp.src('src/scss/**/*.scss')
-        .pipe(plumber({errorHandler: onError}))
+        .pipe(plumber({ errorHandler: onError }))
         .pipe(sass({
             style: 'expanded',
             includePaths: bourbon,
-          includePaths: neat
-      
+            // includePaths: neat
+
         }))
         .pipe(gulp.dest('dist/css'))
         .pipe(rename({
-          suffix: '.min'
+            suffix: '.min'
         }))
         .pipe(nano())
         .pipe(gulp.dest('dist/css'));
@@ -66,62 +99,99 @@ gulp.task('sass', function() {
 // Copy react.js and react-dom.js to assets/js/src/vendor
 // only if the copy in node_modules is "newer"
 gulp.task('copy-react', function() {
-  return gulp.src('node_modules/react/dist/react.js')
-    .pipe(newer('./src/js/vendor/react.js'))
-    .pipe(gulp.dest('./src/js/vendor/'));
+    return gulp.src('node_modules/react/dist/react.js')
+        .pipe(newer('./src/js/vendor/react.js'))
+        .pipe(gulp.dest('./src/js/vendor/'));
 });
 
 gulp.task('copy-react-dom', function() {
-  return gulp.src('node_modules/react-dom/dist/react-dom.js')
-    .pipe(newer('./src/js/vendor/react-dom.js'))
-    .pipe(gulp.dest('./src/js/vendor/'));
+    return gulp.src('node_modules/react-dom/dist/react-dom.js')
+        .pipe(newer('./src/js/vendor/react-dom.js'))
+        .pipe(gulp.dest('./src/js/vendor/'));
 });
 
 
 gulp.task('browserify', function() {
-  return browserify({
-      extensions: ['.jsx', '.js'],
-      debug: true,
-      cache: {},
-      packageCache: {},
-      fullPaths: true,
-      entries: './src/js/main.js',
-    })
-    .transform(babelify.configure({ 
-        presets: ['es2015', 'stage-2', 'react'],
-        ignore: /(bower_components)|(node_modules)/
-    }))
-    .bundle()
-    .pipe(source('all.js'))
-    .pipe(gulp.dest('./dist/js'))
-    .on("error", function (err) { console.log("Error : " + err.message); })
-    
+    return browserify({
+            extensions: ['.jsx', '.js'],
+            debug: true,
+            cache: {},
+            packageCache: {},
+            fullPaths: true,
+            entries: './src/js/main.js',
+        })
+        .transform(babelify.configure({
+            presets: ['es2015', 'stage-2', 'react'],
+            ignore: /(bower_components)|(node_modules)/
+        }))
+        .bundle()
+        .pipe(source('all.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .on("error", function(err) { console.log("Error : " + err.message); })
+
 });
 
 
-gulp.task('image', function () {
-  gulp.src('src/imgs/**/**')
-    .pipe(image())
-    .pipe(gulp.dest('dist/imgs'));
+gulp.task('image', function() {
+    gulp.src('src/imgs/**/**')
+        .pipe(image())
+        .pipe(gulp.dest('dist/imgs'));
 });
 
-gulp.task('browsersync', function(){
+gulp.task('svg', function() {
 
-browserSync.init({
+    var configSVG = {
+        shape: {
+            dimension: { // Set maximum dimensions
+                maxWidth: 32,
+                maxHeight: 32
+            },
+            spacing: { // Add padding
+                padding: 10
+            },
+            dest: 'out/intermediate-svg' // Keep the intermediate files
+        },
+        mode: {
+            view: { // Activate the «view» mode
+                bust: false,
+                render: {
+                    scss: true // Activate Sass output (with default options)
+                }
+            },
+            symbol: true // Activate the «symbol» mode
+        }
+    };
+
+    gulp.src('src/imgs/**/*.svg')
+        .pipe(svgSprite(configSVG))
+        .pipe(gulp.dest('dist/svg'));
+
+});
+
+
+
+
+gulp.task('browsersync', function() {
+
+    browserSync.init({
         proxy: "local.projet"
     });
 
 });
 
-
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('dist/**/*.php', [reload]);
-    gulp.watch('src/js/*.{js,jsx}', [ 'lint', 'browserify', reload]);
+    gulp.watch('src/views/**/**/*.pug', ['pug', reload]);
+    gulp.watch('src/views/data.json', ['pug', reload]);
+    gulp.watch('src/js/*.{js,jsx}', ['lint', 'browserify', reload]);
     gulp.watch('src/scss/*.scss', ['sass', reload]);
     gulp.watch('src/imgs/**/**', ['image', reload]);
+    gulp.watch('src/svg/**/**', ['svg', reload]);
+    gulp.watch('dist/**/**/*.php', [reload]);
 
 });
+
+
 
 
 
@@ -129,6 +199,6 @@ gulp.task('watch', function() {
 
 
 // Default Task
-gulp.task('default', [ 'browserify','lint', 'sass', 'image', 'browsersync', 'watch']);
+gulp.task('default', ['browserify', 'lint', 'sass', 'image', 'svg', 'pug', 'browsersync', 'watch']);
 // Default react
-gulp.task('react', [ 'copy-react','copy-react-dom', 'browserify','lint', 'sass', 'image', 'browsersync', 'watch']);
+gulp.task('react', ['copy-react', 'copy-react-dom', 'browserify', 'lint', 'sass', 'image', 'pug', 'browsersync', 'watch']);
